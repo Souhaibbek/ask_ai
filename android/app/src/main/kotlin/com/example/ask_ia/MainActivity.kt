ed9c531896ffb1c14c4d73800881bc78466993ff
Souhaibbek <Bekkaysouhaib1996@gmail.com>
2023-08-30 13:56:08 +0100

    first_commit

---
+++ android/app/src/main/kotlin/com/example/ask_ia/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ask_ia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Color scaffoldBackgroundColor = const Color(0xFF343541);
+Color cardColor = const Color(0xFF444654);
+String baseUrl = "https://api.openai.com/v1";
+String apiKey = "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";
